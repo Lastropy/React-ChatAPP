62a940e970dbf890623e8d264eae0ac168c64014
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-15 18:56:35 +0530

    Changed file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,20 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
