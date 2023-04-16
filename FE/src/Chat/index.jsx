516633dd0439b8257573346876253af35c60eb4c
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-16 15:54:46 +0530

    Made the Chat component

--- FE/src/Chat/index.jsx
+++ FE/src/Chat/index.jsx
@@ -1,4 +1,23 @@
-const Chat = () => {
+import { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import { ENDPOINT } from "../assets/constants";
+let socket;
+// ✅ location is a prop coming from react router
+// ✅ we are passing data in one component to other component using query string
+// ✅ location.search -> gives our data
+const Chat = ({ location }) => {
+  const [name, setName] = useState();
+  const [room, setRoom] = useState();
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(root);
+
+    socket = io(ENDPOINT);
+    console.log(socket);
+  }, []);
   return <h1>Chat</h1>;
 };
 
