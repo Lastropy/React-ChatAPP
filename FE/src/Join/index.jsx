5c83beae2c73717f0b498a041932eee7d5a6a7fe
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-16 15:03:45 +0530

    Wrote code and CSS for join component

--- FE/src/Join/index.jsx
+++ FE/src/Join/index.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../assets/css/Join.css";
+
 const Join = () => {
-  return <h1>Join</h1>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Enter Name"
+            className="joinInput"
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Room"
+            className="joinInput mt-20"
+            type="text"
+            value={room}
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit" onSubmit={() => {}}>
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
