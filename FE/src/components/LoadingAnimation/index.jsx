25c8ea19a71c69af988470eab0fe5863a1bc2da6
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-22 11:47:25 +0530

    Added loading animation

---
+++ FE/src/components/LoadingAnimation/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/css/loader.css";
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default LoadingAnimation;
