d2407155a477ddbd54e8d977caa134580c195b00
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2023-04-23 13:20:32 +0530

    Implemented emojis in text

--- FE/src/components/InfoBar/index.jsx
+++ FE/src/components/InfoBar/index.jsx
@@ -7,12 +7,16 @@ const InfoBar = ({ roomName }) => {
   return (
     <div className="infoBar">
       <div className="leftInnerContainer">
-        <img className="onlineIcon" src={onlineIcon} />
+        <img
+          className="onlineIcon"
+          src={onlineIcon}
+          alt="online"
+        />
         <h3>{roomName}</h3>
       </div>
       <div className="rightInnerContainer">
         <a href="/">
-          <img src={closeIcon} alt="close-image" />
+          <img src={closeIcon} alt="close" />
         </a>
       </div>
     </div>
--- FE/src/components/Messages/Message/index.jsx
+++ FE/src/components/Messages/Message/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../../assets/css/Message.css";
+import ReactEmoji from "react-emoji";
 
 const Message = ({ key, name, message }) => {
   const { user, text } = message;
@@ -12,13 +13,17 @@ const Message = ({ key, name, message }) => {
         <div className="messageContainer justifyEnd">
           <p className="sentText pr-10">{user}</p>
           <div className="messageBox backgroundBlue">
-            <p className="messageText colorWhite">{text}</p>
+            <p className="messageText colorWhite">
+              {ReactEmoji.emojify(text)}
+            </p>
           </div>
         </div>
       ) : (
         <div className="messageContainer justifyStart">
           <div className="messageBox backgroundLight">
-            <p className="messageText colorDark">{text}</p>
+            <p className="messageText colorDark">
+              {ReactEmoji.emojify(text)}
+            </p>
           </div>
           <p className="sentText pl-10">{user}</p>
         </div>
