0b5094dfaa3c2f8cfc48deb8852d7bb654891a8a
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-12-23 18:27:56 +0530

    Modified Chat Component to fetch messages from db, update client dynamically on room change.

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -5,43 +5,86 @@ import InfoBar from "../components/InfoBar";
 import Input from "../components/Input";
 import Messages from "./Messages";
 import { useAuth0 } from "@auth0/auth0-react";
-let socket;
+import { useNavigate } from "react-router";
 
 const Chat = ({ location }) => {
-	const { user, isAuthenticated } = useAuth0();
-	const { roomName, roomPwd } = queryString.parse(location.search);
+	const { user } = useAuth0();
+	const { roomName, roomPwd, userId } = queryString.parse(location.search);
 	const [messages, setMessages] = useState([]);
 	const [message, setMessage] = useState("");
+	const [socket, setSocket] = useState(undefined);
+	const [room, setRoom] = useState(undefined);
+	const navigate = useNavigate();
 
 	useEffect(() => {
-		if (isAuthenticated) {
-			socket = io(import.meta.env.VITE_SERVER_ENDPOINT);
-			const { name } = user;
-			// Verify before joining
-			socket.emit("join", { name, roomName, roomPwd }, (error) => {
-				if (error) {
-					alert(error);
-					window.history.back();
-				}
-			});
-
-			socket.on("message", (message) => {
-				setMessages(messages.concat(message));
-			});
-
-			return () => {
+		setSocket(io(import.meta.env.VITE_SERVER_ENDPOINT));
+		return () => {
+			if (socket) {
 				socket.disconnect();
 				socket.off();
-			};
+			}
+		};
+	}, []);
+
+	useEffect(() => {
+		if (socket) {
+			socket.emit("joinIfExists:room", { roomName, roomPwd, userUUID: userId }, (arg) => {
+				if (arg.error) {
+					alert("Error while connecting to room.");
+					console.error(arg.error);
+					navigate("/room");
+				} else {
+					alert("Successfully connected to room.");
+					setRoom(arg);
+				}
+			});
 		}
-	}, [location.search, isAuthenticated]);
+	}, [socket, location.search]);
+
+	useEffect(() => {
+		if (room) {
+			alert(`Getting existing messages for ${room.name}`);
+			socket.emit("getForARoom:messages", { roomId: room.id }, (arg) => {
+				if (arg.error) {
+					alert("Error while fetching messages from room.");
+					console.error(arg.error);
+					throw new Error("Error in fetching messages from room");
+				} else {
+					alert("Fetched Messages from room.");
+					setMessages(arg);
+				}
+			});
+		}
+	}, [room]);
+
+	useEffect(() => {
+		if (room) {
+			socket.on("updateRoomClients", (arg) => {
+				alert("Recieving message from other room clients...");
+				console.log(messages, arg);
+				setMessages(messages.concat(arg));
+			});
+		}
+	}, [room, messages]);
 
 	const handleSend = (event) => {
 		event.preventDefault();
-		if (message) {
-			socket.emit("sendMessage", message, () => {
-				setMessage("");
-			});
+		if (!room) alert("Not connected to any room");
+		if (!message) alert("No message to send");
+		if (message && room) {
+			socket.emit(
+				"create:message",
+				{ content: message, roomId: room.id, userId, userName: user.name },
+				(arg) => {
+					if (arg.error) {
+						alert("Error while creating a new message in the room.");
+						console.error(arg.error);
+						throw new Error("Error in creating a new message in the room");
+					} else {
+						setMessage("");
+					}
+				}
+			);
 		}
 	};
 
@@ -50,7 +93,7 @@ const Chat = ({ location }) => {
 			{roomName && roomPwd && user ? (
 				<div className="container">
 					<InfoBar roomName={roomName} />
-					<Messages messages={messages} name={user?.name ?? ""} />
+					<Messages messages={messages} currentUserId={userId} />
 					<Input message={message} handleSend={handleSend} setMessage={setMessage} />
 				</div>
 			) : (
