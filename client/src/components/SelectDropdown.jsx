5a6791a0a65bde502eaca8935c1e13e2404e23cc
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-12-30 10:57:32 +0530

    Added code to disable dropdown if no rooms present

--- client/src/components/SelectDropdown.jsx
+++ client/src/components/SelectDropdown.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SelectDropdown = ({ data, valueField, displayField, onChange }) => {
 	return (
-		<select className="room-select-dropdown" onChange={onChange}>
+		<select className="room-select-dropdown" onChange={onChange} disabled={!data || !data.length}>
 			<option value="" selected disabled hidden>
-				Choose Room Name
+				{!data || !data.length ? "No Room Present" : "Choose Room Name"}
 			</option>
 			{data.map((ele) => (
 				<option value={ele[valueField]}>{ele[displayField]}</option>
